b113d07f2b3a709b6aa2c5ce89b527c75e7e91f8
boburastonov <boburastonov33@gmail.com>
2025-03-07 14:18:28 +0500

    Add dashboard configuration files

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,18 @@
 const ENV = import.meta.env;
 
 const config = {
+  app: {
+    version: ENV.VITE_APP_VERSION,
+    isDev: ENV.VITE_APP_ENV !== "production",
+  },
   api: {
     baseUrl: ENV.VITE_APP_API_URL,
     downloadUrl: `${ENV.VITE_APP_API_URL}/references/download`,
   },
   language: {
-    key: 'language',
-    initial: 'uz',
-    list: ['uz', 'en', 'ru'],
+    key: "language",
+    initial: "uz",
+    list: ["uz", "en", "ru"],
   },
   list: {
     perPage: 10,
